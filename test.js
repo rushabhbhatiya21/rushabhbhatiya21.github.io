b9bbf9d3656366c4dade9842d6d97c54dff5a6e4
rushabhbhatiya21 <134758743+rushabhbhatiya21@users.noreply.github.com>
2024-04-17 16:12:40 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,7 @@
 function fill_data(elementList, valList) {
     console.log("hello");
     for(let i = 0; i < elementList.length; i++) {
+        console.log(elementList[i]);
         document.querySelector('[aria-label="' + elementList[i] + '"]').value = valList[i];
     }
 }
