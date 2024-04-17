140865eba1185393113f91b7f24d4d6ae87a1518
rushabhbhatiya21 <134758743+rushabhbhatiya21@users.noreply.github.com>
2024-04-17 18:06:13 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,8 +1,8 @@
 function fill_data(valList) {
     console.log("hello");
     document.querySelector('[aria-label=" Person Number"').value = valList[0];
-    //var elements = document.querySelector('[aria-label=" dd-mmm-yyyy"');
-    //elements[0].value = valList[1];
-    //elements[1].value = valList[2];
+    var elements = document.querySelector('[aria-label=" dd-mmm-yyyy"');
+    elements[0].value = valList[1];
+    elements[1].value = valList[2];
 }
 
