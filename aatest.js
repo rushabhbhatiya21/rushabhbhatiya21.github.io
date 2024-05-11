c61f0e9ff0e3437f7000f7ab8c2a9c19b37b1d5c
rushabhbhatiya21 <134758743+rushabhbhatiya21@users.noreply.github.com>
2024-05-11 11:06:20 +0530

    Update aatest.js

--- aatest.js
+++ aatest.js
@@ -1,3 +1,19 @@
 function open_mail_box() {
   document.querySelector("#mailboxButton").click();
 }
+
+// Function to wait for an element to be present
+function waitForElement(selector) {
+    return new Promise((resolve, reject) => {
+        // Check if the element is present
+        const element = document.querySelector(selector);
+        if (element) {
+            resolve();
+        } else {
+            // If not present, wait for a short interval and check again
+            setTimeout(() => {
+                waitForElement(selector).then(resolve);
+            }, 1000); // Adjust the interval as needed
+        }
+    });
+}
