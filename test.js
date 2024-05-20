2eaabad023ebb2c796531b7f4c85c7784e6bd250
rushabhbhatiya21 <134758743+rushabhbhatiya21@users.noreply.github.com>
2024-05-20 14:18:53 +0530

    Update test.js

--- test.js
+++ test.js
@@ -330,7 +330,8 @@ async function add_new_row_below() {
     document.querySelector('img[id*="ctb1\\:\\:icon"]').click();
 }
 
-function scroll_down() {
+function scroll_up_down() {
+    document.querySelector('[id*="AT2\\:_ATp\\:ATt2\\:\\:vscroller"]').scrollBy(0, 0);
     document.querySelector('[id*="AT2\\:_ATp\\:ATt2\\:\\:vscroller"]').scrollBy(0, Number.MAX_SAFE_INTEGER);
 }
 
@@ -360,8 +361,8 @@ async function fill_row_data(project, task, exType, hourList) {
             cardState["rowNo"] = index+1;
             await add_new_row_below();
             // console.log("index after adding row below: ", index);
-            await delay(2000);
-            scroll_down();
+            await delay(4000);
+            scroll_up_down();
             await delay(1000);
             resolve();
         }).catch((error) => {
