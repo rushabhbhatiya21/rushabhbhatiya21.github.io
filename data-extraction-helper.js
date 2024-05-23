6be86e64bd3339cf2e64ab8fdba77f93e9c07548
rushabhbhatiya21 <134758743+rushabhbhatiya21@users.noreply.github.com>
2024-05-23 09:53:40 +0530

    Update data-extraction-helper.js

--- data-extraction-helper.js
+++ data-extraction-helper.js
@@ -78,9 +78,9 @@ function cancel_action() {
 //output - none
 function fill_person_data(person_data) {
   person_data = person_data.split('~');
-  document.querySelector('input[id*="qryId1\\:value10\\:\\:content"]').value = person_data[0]; //person number
-  document.querySelector('input[id*="qryId1\\:value40\\:\\:content"]').value = person_data[1]; //from date
-  document.querySelector('input[id*="qryId1\\:value50\\:\\:content"]').value = person_data[2]; //to date
+  document.querySelector('input[id*="qryId1\\:value10\\:\\:content"]').value = person_data[0].toString(); //person number
+  document.querySelector('input[id*="qryId1\\:value40\\:\\:content"]').value = person_data[1].toString(); //from date
+  document.querySelector('input[id*="qryId1\\:value50\\:\\:content"]').value = person_data[2].toString(); //to date
   document.querySelector('button[id*="qryId1\\:\\:search"]').click(); //click on search
   current_person_data = [...person_data];
 }
